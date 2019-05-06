c69666a07782746254eb6270b418edc2f1dee859
Todd <mrtsart@gmail.com>
2019-05-06 10:43:42 -0500

    Update Chp1.1.py

--- Chp1.1.py
+++ Chp1.1.py
@@ -267,3 +267,62 @@ print('You will be ' + str(int(myAge) + 1) + ' in a year.')
 # True
 
 
+### Practice Questions
+
+
+### Q:1. Which of the following are operators, and which are values?
+
+# Operators: * - / +
+# Values: 'hello' 88.8 5
+
+
+### Q: 2. Which of the following is a variable, and which is a string?
+
+# Variable: spam
+# String: 'spam'
+
+### Q: 3. Name three data types.
+
+# Integer, String, Float
+
+### Q: 4. What is an expression made up of? What do all expressions do?
+
+#
+
+### Q: 5. This chapter introduced assignment statements, like spam = 10. 
+# # What is the difference between an expression and a statement?
+
+# 
+
+
+### Q: 6. What does the variable bacon contain after the following code runs?
+
+bacon = 20
+bacon + 1
+
+# bacon = 21
+
+### Q: 7. What should the following two expressions evaluate to?
+
+'spam' + 'spamspam'
+# 'spamspamspam'
+
+'spam' * 3
+# 'spamspamspam'
+
+### Q: 8. Why is eggs a valid variable name while 100 is invalid?
+
+# 100 begins with a number, eggs does not
+
+### Q: 9. What three functions can be used to get the integer, 
+# floating-point number, or string version of a value?
+
+
+### Q: 10. Why does this expression cause an error? How can you fix it?
+
+# you can not add an integer to a string before converting it to a string
+
+### Extra Credit: round() function
+
+
+
