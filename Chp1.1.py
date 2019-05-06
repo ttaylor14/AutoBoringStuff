023b6cb838a7ac7ddff867ffa221f309bfe75308
Todd <mrtsart@gmail.com>
2019-05-06 07:55:09 -0500

    Create Chp1.1.py

---
+++ Chp1.1.py
@@ -0,0 +1,47 @@
+# Introduction to Python
+
+# going through the basics taught in Chapter 1
+
+2 + 2
+# output should be 4
+
+2
+
+# out put will be 2
+
+2 ** 3      # 8 - exponent
+22 % 8      # 6 - modulus/remainder
+22 // 8     # 2 - Integer Division/Floored quotient
+22 / 8      # 2.75 - Division
+3 * 5       # 15 - Multiplication
+5 - 2       # 3 - subtraction
+2 + 2       # 4 - Addition
+
+
+# Python follows Order of Operations (precedence)
+
+
+2 + 3 * 6
+# 20
+(2 + 3) * 6
+# 30
+48565878 * 578453
+# 28093077826734
+2 ** 8
+# 256
+23 / 7
+# 3.2857142857142856
+23 // 7
+# 3
+23 % 7
+# 2
+2     +            2
+# 4
+(5 - 1) * ((7 + 1) / (3 - 1))
+# 16.0
+
+# 5 + 
+# # this will result in an error because 5 has not been added to anything
+
+# 42 + 5 + * 2 
+# # This will result in an error because two operations are given in a row
