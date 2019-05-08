c99070b864506ababa0365480b97110cfb20cdef
Todd <mrtsart@gmail.com>
2019-05-07 20:19:34 -0500

    Update Chp2.1.py

--- Chp2.1.py
+++ Chp2.1.py
@@ -1,5 +1,99 @@
 # Introduction to Python
 
-# Chapter 2 – Flow Control
+# Chapter 2  Flow Control
 # automatetheboringstuff.com/chapter2/
+
+### Boolean Values
+
+spam = True
+spam
+
+# True # true is not defined
+
+# true = 2 + 2 # You can not assign to keyword
+
+
+
+### Comparison Operators
+
+'''
+Operator
+
+==
+!=
+<
+>
+<=
+>=
+
+
+Meaning
+
+Equal to
+Not equal to
+Less than
+Greater than
+Less than or equal to
+Greater than or equal to
+
+These operators evaluate to True or False depending on the values you give them. 
+Let’s try some operators now, starting with == and !=.
+
+'''
+
+42 == 42
+42 == 99
+
+# True
+# False
+
+2 != 3
+# True
+
+2 != 2
+# False
+
+'hello' == 'hello'
+'hello' == 'Hello'
+# True
+# False
+
+'dog' != 'cat'
+# True
+
+True == True
+# True
+
+True != False
+# True
+
+42 == 42.0
+# True
+
+42 == '42'
+# False
+
+
+42 < 100
+42 > 100
+# True
+# False
+
+42 < 42
+# False
+
+eggCount = 42
+eggCount <= 42
+# True
+
+myAge = 29
+myAge >= 10
+# True
+
+### Boolean Operators
+
+
+
+
+
 
